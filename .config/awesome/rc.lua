41090700ebac26fef69d517f9fda7aae3ec8562d
Johan Kiviniemi <devel@johan.kiviniemi.name>
2009-06-07 17:57:27 +0300

    awesome: Disable some of the layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -36,14 +36,14 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
+    --awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
+    --awful.layout.suit.tile.top,
+    --awful.layout.suit.fair,
+    --awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
+    --awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.magnifier,
     awful.layout.suit.floating
 }
 
